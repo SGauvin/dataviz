61f212468adb275b7fae7ede0e36ef12ea2ed702
Simon Gauvin <gauvinsimon@gmail.com>
2021-06-21 21:46:42 -0400

    Ye

--- src/preprocess.js
+++ src/preprocess.js
@@ -75,9 +75,9 @@ function computeDerivedStats(data) {
 }
 
 export async function getLiguaStats() {
-    const standardStatsFilename = window.location.origin + '/standard_stats_la_ligua.csv';
-    const shootingStatsFilename = window.location.origin + '/shooting_stats_la_ligua.csv';
-    const passingStatsFilename = window.location.origin + '/passing_stats_la_ligua.csv';
+    const standardStatsFilename = 'standard_stats_la_ligua.csv';
+    const shootingStatsFilename = 'shooting_stats_la_ligua.csv';
+    const passingStatsFilename = 'passing_stats_la_ligua.csv';
     const standardStats = await d3.csv(standardStatsFilename);
     const shootingStats = await d3.csv(shootingStatsFilename);
     const passingStats = await d3.csv(passingStatsFilename);
@@ -94,9 +94,9 @@ export async function getLiguaStats() {
 }
 
 export async function getBenzemaHistoricalStats() {
-    const standardStatsFilename = window.location.origin + '/benzema_standard_stats_historical_values.csv';
-    const shootingStatsFilename = window.location.origin + '/benzema_shooting_stats_historical_values.csv';
-    const passingStatsFilename = window.location.origin + '/benzema_passing_stats_historical_values.csv';
+    const standardStatsFilename = 'benzema_standard_stats_historical_values.csv';
+    const shootingStatsFilename = 'benzema_shooting_stats_historical_values.csv';
+    const passingStatsFilename = 'benzema_passing_stats_historical_values.csv';
     const standardStats = await d3.csv(standardStatsFilename);
     const shootingStats = await d3.csv(shootingStatsFilename);
     const passingStats = await d3.csv(passingStatsFilename);
@@ -112,9 +112,9 @@ export async function getBenzemaHistoricalStats() {
 }
 
 export async function getGiroudHistoricalStats() {
-    const standardStatsFilename = window.location.origin + '/giroud_standard_stats_historical_values.csv';
-    const shootingStatsFilename = window.location.origin + '/giroud_shooting_stats_historical_values.csv';
-    const passingStatsFilename = window.location.origin + '/giroud_passing_stats_historical_values.csv';
+    const standardStatsFilename = 'giroud_standard_stats_historical_values.csv';
+    const shootingStatsFilename = 'giroud_shooting_stats_historical_values.csv';
+    const passingStatsFilename = 'giroud_passing_stats_historical_values.csv';
     const standardStats = await d3.csv(standardStatsFilename);
     const shootingStats = await d3.csv(shootingStatsFilename);
     const passingStats = await d3.csv(passingStatsFilename);
